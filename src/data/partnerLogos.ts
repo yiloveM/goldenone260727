7d2605a6a43fc1a3d9699ca4c3097f5e7e56db65
一哥 <chinapool@yeah,net>
2026-08-11 18:14:47 +0800

    Add partner brands section

---
+++ src/data/partnerLogos.ts
@@ -0,0 +1,9 @@
+/** Replace these temporary MIT-licensed Heroicons URLs with approved partner marks before launch. */
+export const partnerLogos: Array<{ name: string; logo: string }> = [
+  { name: 'Gift', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/gift.svg' },
+  { name: 'Heart', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/heart.svg' },
+  { name: 'Sparkles', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/sparkles.svg' },
+  { name: 'Star', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/star.svg' },
+  { name: 'Swatch', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/swatch.svg' },
+  { name: 'Trophy', logo: 'https://raw.githubusercontent.com/tailwindlabs/heroicons/master/src/24/solid/trophy.svg' },
+];
